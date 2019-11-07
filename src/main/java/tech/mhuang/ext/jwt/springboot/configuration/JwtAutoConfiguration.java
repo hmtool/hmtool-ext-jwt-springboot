db784dc74c8cb58f6acc3d57179bbf426fb03159
mhuang <mhuang_email@163.com>
2019-11-07 09:16:19 +0800

    1.3.0

--- src/main/java/tech/mhuang/ext/jwt/springboot/configuration/JwtAutoConfiguration.java
+++ src/main/java/tech/mhuang/ext/jwt/springboot/configuration/JwtAutoConfiguration.java
@@ -3,9 +3,8 @@ package tech.mhuang.ext.jwt.springboot.configuration;
 import tech.mhuang.core.check.CheckAssert;
 import tech.mhuang.ext.jwt.admin.JwtFramework;
 import tech.mhuang.ext.jwt.admin.external.IJwtExternal;
-import tech.mhuang.ext.jwt.admin.external.IJwtProducer;
 import tech.mhuang.ext.spring.start.SpringContextHolder;
-import tech.mhuang.ext.springboot.context.ContextAutoConfiguration;
+import tech.mhuang.ext.springboot.context.SpringBootExtAutoConfiguration;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -26,7 +25,7 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnClass(JwtFramework.class)
 @EnableConfigurationProperties(JwtProperties.class)
 @ConditionalOnProperty(prefix = "mhuang.jwt",name = "enable",havingValue = "true",matchIfMissing = true)
-@AutoConfigureAfter({ContextAutoConfiguration.class})
+@AutoConfigureAfter({SpringBootExtAutoConfiguration.class})
 @Slf4j
 public class JwtAutoConfiguration {
 
